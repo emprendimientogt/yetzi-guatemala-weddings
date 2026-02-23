103a10a94c154b0c5728ca0834989bd330273815
Tanya Muhun <tanya.muhun@wotdev.com>
2026-02-22 22:03:34 -0600

    base diferente
    base /

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/yetzi-guatemala-weddings/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
