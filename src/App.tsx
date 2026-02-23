bfcf07bbd7c14ae29d97df262c75dba6fa48536f
Tanya Muhun <tanya.muhun@wotdev.com>
2026-02-22 22:02:43 -0600

    prueba
    prueba router diferente

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/yetzi-guatemala-weddings">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/yetzi-guatemala-weddings/" : "/",
+  base: "/yetzi-guatemala-weddings/",
   server: {
     host: "::",
     port: 8080,
